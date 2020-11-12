a9e955e4e6f2efb5f115296da723b23df9dd135c
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-11 20:35:01 -0800

    Add sold out badge
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ puts "Seeding Data ..."
 
 # Helper functions
 def open_asset(file_name)
-  File.open(Rails.root.join('db', 'seed_assets', file_name))
+  File.open(Rails.root.join("db", "seed_assets", file_name))
 end
 
 # Only run on development (local) instances not on production, etc.
@@ -25,9 +25,9 @@ end
 
 puts "Finding or Creating Categories ..."
 
-cat1 = Category.find_or_create_by! name: 'Apparel'
-cat2 = Category.find_or_create_by! name: 'Electronics'
-cat3 = Category.find_or_create_by! name: 'Furniture'
+cat1 = Category.find_or_create_by! name: "Apparel"
+cat2 = Category.find_or_create_by! name: "Electronics"
+cat3 = Category.find_or_create_by! name: "Furniture"
 
 ## PRODUCTS
 
@@ -36,101 +36,99 @@ puts "Re-creating Products ..."
 Product.destroy_all
 
 cat1.products.create!({
-  name:  'Men\'s Classy shirt',
+  name: 'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel1.jpg'),
+  image: open_asset("apparel1.jpg"),
   quantity: 10,
-  price: 64.99
+  price: 64.99,
 })
 
 cat1.products.create!({
-  name:  'Women\'s Zebra pants',
+  name: 'Women\'s Zebra pants',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel2.jpg'),
+  image: open_asset("apparel2.jpg"),
   quantity: 18,
-  price: 124.99
+  price: 124.99,
 })
 
 cat1.products.create!({
-  name:  'Hipster Hat',
+  name: "Hipster Hat",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel3.jpg'),
+  image: open_asset("apparel3.jpg"),
   quantity: 4,
-  price: 34.49
+  price: 34.49,
 })
 
 cat1.products.create!({
-  name:  'Hipster Socks',
+  name: "Hipster Socks",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel4.jpg'),
+  image: open_asset("apparel4.jpg"),
   quantity: 8,
-  price: 25.00
+  price: 25.00,
 })
 
 cat1.products.create!({
-  name:  'Russian Spy Shoes',
+  name: "Russian Spy Shoes",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel5.jpg'),
+  image: open_asset("apparel5.jpg"),
   quantity: 8,
-  price: 1_225.00
+  price: 1_225.00,
 })
 
 cat1.products.create!({
-  name:  'Human Feet Shoes',
+  name: "Human Feet Shoes",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel6.jpg'),
+  image: open_asset("apparel6.jpg"),
   quantity: 82,
-  price: 224.50
+  price: 224.50,
 })
 
-
 cat2.products.create!({
-  name:  'Modern Skateboards',
+  name: "Modern Skateboards",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics1.jpg'),
+  image: open_asset("electronics1.jpg"),
   quantity: 40,
-  price: 164.49
+  price: 164.49,
 })
 
 cat2.products.create!({
-  name:  'Hotdog Slicer',
+  name: "Hotdog Slicer",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics2.jpg'),
+  image: open_asset("electronics2.jpg"),
   quantity: 3,
-  price: 26.00
+  price: 26.00,
 })
 
 cat2.products.create!({
-  name:  'World\'s Largest Smartwatch',
+  name: 'World\'s Largest Smartwatch',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics3.jpg'),
+  image: open_asset("electronics3.jpg"),
   quantity: 32,
-  price: 2_026.29
+  price: 2_026.29,
 })
 
 cat3.products.create!({
-  name:  'Optimal Sleeping Bed',
+  name: "Optimal Sleeping Bed",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture1.jpg'),
+  image: open_asset("furniture1.jpg"),
   quantity: 320,
-  price: 3_052.00
+  price: 3_052.00,
 })
 
 cat3.products.create!({
-  name:  'Electric Chair',
+  name: "Electric Chair",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture2.jpg'),
+  image: open_asset("furniture2.jpg"),
   quantity: 2,
-  price: 987.65
+  price: 987.65,
 })
 
 cat3.products.create!({
-  name:  'Red Bookshelf',
+  name: "Red Bookshelf",
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture3.jpg'),
+  image: open_asset("furniture3.jpg"),
   quantity: 23,
-  price: 2_483.75
+  price: 2_483.75,
 })
-
 
 puts "DONE!"
