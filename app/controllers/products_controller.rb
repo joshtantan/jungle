bfa7f7603ee0d577f2a4516fe04efaa466ef9914
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-11 16:55:34 -0800

    Standardize price display with $ and 2 decimal points
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,4 @@
 class ProductsController < ApplicationController
-
   def index
     @products = Product.all.order(created_at: :desc)
   end
@@ -7,5 +6,4 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find params[:id]
   end
-
 end
