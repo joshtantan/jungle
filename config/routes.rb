0eceb3e0449ea16917ff5b7c109f63509b4583da
Joshua Tan <josh.tan.tann@gmail.com>
2020-11-12 13:13:43 -0800

    Implement About page
    Signed-off-by: Joshua Tan <josh.tan.tann@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-
-  root to: 'products#index'
+  root to: "products#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  get "about" => "about#show"
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
   end
 
